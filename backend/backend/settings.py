8afe27ad97c625e0075cc3feaed1dfa9e9476dc1
Dan Hayworth <dan.hayworth@hotmail.com>
2021-08-22 19:58:36 +1200

    added JWT

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -94,7 +94,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication')
 }
 
 
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from posts.views import PostViewSet as PostView
 from users.views import UserViewSet as UserView
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = routers.DefaultRouter()
 router.register(r'api/v1/users', UserView)
@@ -12,5 +13,7 @@ router.register(r'api/v1/posts', PostView)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/token/', TokenObtainPairView.as_View()),
+    path('api/token/refresh', TokenRefreshView.as_View()),
 ]
