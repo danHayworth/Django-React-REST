00a0124c5a4b616995a8ecfe8b8a1238141dac10
Dan Hayworth <dan.hayworth@hotmail.com>
2021-08-25 14:56:38 +1200

    added get user list and detail view

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,17 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from posts.views import PostViewSet as PostView
-from users.views import UserViewSet, UserView
+from users.views import user_list, UserView, user_detail
 from users.views import RegisterView, LoginView, LogoutView
-from rest_framework import routers
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.schemas import get_schema_view
 from rest_framework.documentation import include_docs_urls
 
-router = routers.DefaultRouter()
-router.register('api/v1/users', UserViewSet)
-router.register('api/v1/posts', PostView)
 
 API_TITLE = 'Posts & Users API'
 API_DESCRIPTION = 'A Web API for creating and viewing posts and users'
@@ -20,7 +16,9 @@ schema_view = get_schema_view(title=API_TITLE)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('api/v1/users', user_list),
+    path('api/v1/users/<int:pk>', user_detail),
+    path('api/v1/posts', PostView),
     path('login/', LoginView.as_view()),
     path('register/', RegisterView.as_view()),
     path('user/', UserView.as_view()),
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from rest_framework import permissions
-#from rest_framework.serializers import Serializer
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from backend.serializers import UserSerializer
@@ -9,11 +8,45 @@ from .models import CustomUser
 from rest_framework.views import APIView
 import jwt, datetime
 from django.conf import settings
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = CustomUser.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated,])
+def user_list(request):
+
+    if request.method == 'GET':
+        users = CustomUser.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticated,])
+def user_detail(request, pk):
+    try:
+        user = CustomUser.objects.get(pk=pk)
+    except CustomUser.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class RegisterView(APIView):
     def post(self, request, *args, **kwargs):
